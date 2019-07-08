0d683c0357cdea618895c3726e9c8e8a82ea2c82
rummolprod999 <rummolprod999@gmail.com>
2019-07-08 12:46:50 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
 
+class Item {
+  purchase: string;
+  done: boolean;
+  price: number;
+
+  constructor(purchase: string, price: number) {
+
+    this.purchase = purchase;
+    this.price = price;
+    this.done = false;
+  }
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'purchase-app',
+  template: `<div class="page-header">
+        <h1> Список покупок </h1>
+    </div>
+    <div class="panel">
+        <div class="form-inline">
+            <div class="form-group">
+                <div class="col-md-8">
+                    <input class="form-control" [(ngModel)]="text" placeholder = "Название" />
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6">
+                    <input type="number" class="form-control" [(ngModel)]="price" placeholder="Цена" />
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-offset-2 col-md-8">
+                    <button class="btn btn-default" (click)="addItem(text, price)">Добавить</button>
+                </div>
+            </div>
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Предмет</th>
+                    <th>Цена</th>
+                    <th>Куплено</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr *ngFor="let item of items">
+                    <td>{{item.purchase}}</td>
+                    <td>{{item.price}}</td>
+                    <td><input type="checkbox" [(ngModel)]="item.done" /></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>`,
+  styles: [`h1, h2{color:navy;}
+            p{font-size:13px; font-family:Verdana;}`]
 })
 export class AppComponent {
-  title = 'FirsAngularProject';
+  items: Item[] =
+    [
+      { purchase: "Хлеб", done: false, price: 15.9 },
+      { purchase: "Масло", done: false, price: 60 },
+      { purchase: "Картофель", done: true, price: 22.6 },
+      { purchase: "Сыр", done: false, price:310 }
+    ];
+  addItem(text: string, price: number): void {
+
+    if(text==null || text.trim()=="" || price==null)
+      return;
+    this.items.push(new Item(text, price));
+  }
 }
