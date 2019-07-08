d4f080360c958aacafbfa8bd0fea6f57d137bc2a
rummolprod999 <rummolprod999@gmail.com>
2019-07-08 14:23:54 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,59 +15,22 @@ class Item {
 
 @Component({
   selector: 'purchase-app',
-  template: `<div class="page-header">
-        <h1> Список покупок </h1>
-    </div>
-    <div class="panel">
-        <div class="form-inline">
-            <div class="form-group">
-                <div class="col-md-8">
-                    <input class="form-control" [(ngModel)]="text" placeholder = "Название" />
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-6">
-                    <input type="number" class="form-control" [(ngModel)]="price" placeholder="Цена" />
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-offset-2 col-md-8">
-                    <button class="btn btn-default" (click)="addItem(text, price)">Добавить</button>
-                </div>
-            </div>
-        </div>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Предмет</th>
-                    <th>Цена</th>
-                    <th>Куплено</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr *ngFor="let item of items">
-                    <td>{{item.purchase}}</td>
-                    <td>{{item.price}}</td>
-                    <td><input type="checkbox" [(ngModel)]="item.done" /></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>`,
-  styles: [`h1, h2{color:navy;}
-            p{font-size:13px; font-family:Verdana;}`]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   items: Item[] =
     [
-      { purchase: "Хлеб", done: false, price: 15.9 },
-      { purchase: "Масло", done: false, price: 60 },
-      { purchase: "Картофель", done: true, price: 22.6 },
-      { purchase: "Сыр", done: false, price:310 }
+      { purchase: 'Хлеб', done: false, price: 15.9 },
+      { purchase: 'Масло', done: false, price: 60 },
+      { purchase: 'Картофель', done: true, price: 22.6 },
+      { purchase: 'Сыр', done: false, price: 310 }
     ];
   addItem(text: string, price: number): void {
 
-    if(text==null || text.trim()=="" || price==null)
+    if (text == null || text.trim() == '' || price == null) {
       return;
+    }
     this.items.push(new Item(text, price));
   }
 }
